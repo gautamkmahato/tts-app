473925f4773316a04eb804f5b79c5bb382b4463f
gautamkmahato <gautamkmahato@gmail.com>
2025-02-28 15:16:25 +0530

    changed the webhook route

--- app/action/createNewUser.ts
+++ app/action/createNewUser.ts
@@ -1,7 +1,7 @@
 'use server'
 
 export default async function createNewUser(inputData: unknown){
-    const response = await fetch('http://localhost:5000/api/v1/user', {
+    const response = await fetch('https://tts-backend.vercel.app/api/v1/user', {
         method: 'POST',
         headers: {
             'Content-type': 'application/json' 
